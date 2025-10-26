00c607c42095464c8b30338f013fbb3873ce3cec
cheave2 <cheave2@wgu.edu>
2025-10-26 15:20:56 -0500

    Moved to a UserControl builder for nav items instead of individual controls for each nav element

---
+++ UserControls/NavItem.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Omnis.UserControls
+{
+    /// <summary>
+    /// Interaction logic for NavItem.xaml
+    /// </summary>
+    public partial class NavItem : UserControl
+    {
+        public NavItem()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TitleProperty = 
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(NavItem), new PropertyMetadata(string.Empty));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty IconSourceProperty = 
+            DependencyProperty.Register(nameof(IconSource), typeof(ImageSource), typeof(NavItem), new PropertyMetadata(null));
+
+        public ImageSource IconSource
+        {
+            get => (ImageSource)GetValue(IconSourceProperty);
+            set => SetValue(IconSourceProperty, value);
+        }
+    }
+}
